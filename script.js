6a04c6817c10df68654af993de607198274e5f0c
davebryan001 <dave.tamakloe@gmail.com>
2024-05-29 04:55:09 +0000

    my website

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function downloadCV() {
+    const cvUrl ="/assets/DaveBryan_Tamakloe_CV.pd"
+}
